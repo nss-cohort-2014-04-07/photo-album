80867e4942311b3e6bb3a6ee00dd81e03590e9c4
Chyld Medford <chyld.medford@gmail.com>
2014-05-09 11:56:16 -0500

    done

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -15,9 +15,13 @@ module.exports = (req, res, next)=>{
 
 function load(app, fn){
   var home = traceur.require(__dirname + '/../routes/home.js');
+  var albums = traceur.require(__dirname + '/../routes/albums.js');
 
   app.get('/', dbg, home.index);
   app.get('/help', dbg, home.help);
+  app.get('/albums', dbg, albums.index);
+  app.post('/albums', dbg, albums.create);
+  app.get('/albums/:id', dbg, albums.show);
   console.log('Routes Loaded');
   fn();
 }
